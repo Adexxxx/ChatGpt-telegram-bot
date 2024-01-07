9049646a226827ad0cc83b8a33d2b70ac066c0b6
Adexxxx <108570539+Adexxxx@users.noreply.github.com>
2024-01-07 11:11:52 +0300

    Update main.py

--- wihout context/main.py
+++ wihout context/main.py
@@ -2,24 +2,21 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import Message
 from ChatGPT import gpt
     
-TOKEN = '6702860791:AAHXXeO5rBW_hhbC5f3EZaHQpl5XI4mitww'
+TOKEN = 'YOUR_TOKEN'        #Токен телеграм бота
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
     
 
-@dp.message_handler(commands='start')
+@dp.message_handler(commands='start')            #Обработка команды /start
 async def start(message: Message):
-    with open('C:\\pythonProject\\cgpt_bot\\gptu.txt', 'a', encoding='utf-8') as file:
-        user = message.from_user
-        file.write(f'Имя: "{user.first_name}"; Фамилия: "{user.last_name}"; тег: "@{user.username}"; время: {message.date}; id: {message.from_user.id}"\n')
     await message.answer('Привет, это чат-бот на основе модели gpt 3.5🔥\nПриступим!')
 
 
-@dp.message_handler(content_types=types.ContentType.TEXT)
+@dp.message_handler(content_types=types.ContentType.TEXT)        #Любой текст = запрос к chatgpt. 
 async def mes(message: types.Message): 
-    await message.answer('Генерируется ответ♻️')
-    await message.reply(gpt(message.text)) # type: ignore
-    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
+    await message.answer('Генерируется ответ♻️')            #Даём понять пользователю, что бот работает
+    await message.reply(gpt(message.text)) # type: ignore        Тут запрос принимается, отдаётся на обработку и выводится
+    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)        #Удаляем первое сообщение. Только для эстетики
     
-if __name__ == '__main__':
+if __name__ == '__main__':            #Запускаем проверку поступающих в бота сообщений.
     executor.start_polling(dp)
