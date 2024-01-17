def84ae573da3d04b637ed836615f68856db0c7a
CryZFix <cryzfix@gmail.com>
2024-01-17 16:45:16 +0400

    Refactoring and adding Asynchrony

--- with context/ChatGPT.py
+++ with context/ChatGPT.py
@@ -1,66 +1,73 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 import sqlite3
 import re
-import requests
 
-TOKEN = 'YOUR_BOT_API_KEY'                            # Вставляем свои токены от бота и от чат гпт
-client = OpenAI(api_key='YOUR_OPENAI_API_KEY')
+client = AsyncOpenAI(api_key='YOUR_OPENAI_API_KEY')
 
-def gpt(text: str, id: int, m_id: int):
+
+async def gpt(text: str, user_id: int) -> str | bool:
     try:
-        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={id}&text=Генерируется ответ♻️&reply_to_message_id={m_id}"    # Даём юзеру понять, что приняли его сообщение
-        data = requests.get(url).json()
-        rl = f"https://api.telegram.org/bot{TOKEN}/sendChatAction?chat_id={id}&action=typing"     # Для визуала делаем действие боту, типо он печатает, как реальный человек
-        ata = requests.get(rl).json()
-        
-        connect = sqlite3.connect('gpt_us.db')        # Подключаемся к бд, чтобы достать последние два ответа бота. Они будут нужны для чат гпт
-        cursor = connect.cursor()
-        sql_query = f"""
-                                SELECT con, con2 FROM ChatGPT WHERE id = '{id}'
-                                """
-        con = list(cursor.execute(sql_query).fetchone())[0]        # Берём два последних сообщения
-        con2 = list(cursor.execute(sql_query).fetchone())[1]
+        """Подключаемся к бд, чтобы достать последние два ответа бота. Они будут нужны для чат гпт"""
 
-        # Передаём инфу к чат гпт
-        completion = client.chat.completions.create(
-        model = 'gpt-3.5-turbo',        # Версию можно менять по своему усмотрению
-        messages = [
-            {"role": "system", "content" : "chipi chipi chapa chapa"},        # В поле content указываем "личность" бота на английском языке
-            {'role': 'user', 'content': f'{text}'},        # Здесь передаётся сам запрос
-            {'role': 'assistant', 'content': f'{con} {con2}'}    # Передаём контекст - последние два ответа бота
-        ],
-        temperature = 0.9        # Значение от 0 до 1. Отвечает за количество выразительности
-        )
-        ppp = re.compile('[a-zA-Z]')        # Нужно для проверки дальше
+        connect = sqlite3.connect("gpt_us.db", check_same_thread=False)
+        cursor = connect.cursor()
+        result = cursor.execute("SELECT con, con2 FROM ChatGPT WHERE id = ?", (user_id,)).fetchone()
+        if result:
+            con = result[0]
+            con2 = result[1]
+        else:
+            con = ""
+            con2 = ""
+
+        """
+        Передаём инфу к чат гпт
         
-        english_text = completion.choices[0].message.content        # Получаем ответ нейросети на сообщение пользователя
-        if ppp.match(english_text):         # Проверка на английский язык. Если нейронка вывела ответ на англе, то, возможно, это баг(так как запрос был, вероятнее всего на русском).    
-            completion = client.chat.completions.create(        # Если ответ всё таки на английском, то прогоняем вновь, только без контекста. Поразительно, но работает.
-            model = 'gpt-3.5-turbo',
-            messages = [
-                {"role": "system", "content" : "chipi chipi chapa chapa"},
-                {'role': 'user', 'content': f'{text}'}
+        Поле "model" версию можно менять по своему усмотрению
+        В поле "role:system" в content указываем "личность" бота на английском языке
+        В поле "role:user" в content передаем текст запроса от пользователя
+        В поле "role:assistant" в content передаём контекст - последние два ответа бота
+        Поле "temperature" отвечает за количество выразительности ответа. Значение от 0 до 1. 
+        """
+
+        completion = await client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "chipi chipi chapa chapa"},
+                {"role": "user", "content": f"{text}"},
+                {"role": "assistant", "content": f"{con} {con2}"}
             ],
-            temperature = 0.9
+            temperature=0.9
+        )
+        ppp = re.compile("[a-zA-Z]")  # Нужно для проверки дальше
+        
+        english_text = completion.choices[0].message.content  # Получаем ответ нейросети на сообщение пользователя
+        if ppp.match(english_text):  # Проверяем язык ответа, если Английский запрашиваем повторно без контекста
+            completion = await client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "chipi chipi chapa chapa"},
+                    {"role": "user", "content": f"{text}"}
+                ],
+                temperature=0.9
             )
             english_text = completion.choices[0].message.content
 
         try:
-            sql_query = f"UPDATE ChaTGPT SET con = '{english_text}', con2 = '{con}' WHERE id = {id}"        # После обработки запроса обновляем ответы для контекста в бд 
-            cursor.execute(sql_query)   
-        except sqlite3.OperationalError:        # На случай ошибок
-            sql_query = f"UPDATE ChaTGPT SET con = '', con2 = '{con}' WHERE id = {id}"
-            cursor.execute(sql_query) 
-        connect.commit()        # Сохраняем и закрываем бд
+            """После обработки запроса, обновляем ответы для контекста в бд"""
+
+            cursor.execute(
+                "UPDATE ChaTGPT SET con = ?, con2 = ? WHERE id = ?",
+                (english_text, con, user_id)
+            )
+        except sqlite3.OperationalError:  # На случай ошибок
+            cursor.execute(
+                "UPDATE ChaTGPT SET con = ?, con2 = ? WHERE id = ?",
+                ("", con, user_id)
+            )
+        connect.commit()  # Сохраняем и закрываем бд
         connect.close()
         
-        urll = f"https://api.telegram.org/bot{TOKEN}/deleteMessage?chat_id={id}&message_id={data['result']['message_id']}"        # Удаляем сообщение о принятии запроса пользователя
-        print(requests.get(urll).json()) 
-        
-        mes = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={id}&text={english_text}&parse_mode=Markdown&reply_to_message_id={m_id}"        # Отсылаем юзеру ответ на его запрос
-        print(requests.get(mes).json()) 
-    except:        # На случай превышения лимитов запросов к api
-        mess = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={id}&text=❌ Что-то пошло не так. Попробуй снова через 30 секунд&parse_mode=Markdown&reply_to_message_id={m_id}"
-        print(requests.get(mess).json()) 
-
-
+        return english_text
+    except Exception as ex:  # На случай превышения лимитов запросов к api
+        print(ex)
+        return False
--- with context/main.py
+++ with context/main.py
@@ -1,68 +1,76 @@
+import sqlite3
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import Message
 from ChatGPT import gpt
-import sqlite3
-import threading
-    
-TOKEN = 'YOUR_API_KEY'
-bot = Bot(TOKEN)            
+
+TOKEN = "YOUR_BOT_API_KEY"
+bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
 
-def create_table():                        # Создаём бд, если её ещё нет
-    connect = sqlite3.connect('gpt_us.db')
+def create_table():
+    """Создаём бд, если её ещё нет"""
+
+    connect = sqlite3.connect("gpt_us.db", check_same_thread=False)
     cursor = connect.cursor()
 
-    sql_query = """
-                    CREATE TABLE IF NOT EXISTS ChatGPT (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS ChatGPT (
                     id INT,
                     con TEXT,
                     con2 TEXT
-                    )
-                    """
-    cursor.execute(sql_query)
-    connect.commit()
-    connect.close()    
-
-
-@dp.message_handler(commands='start')            # Обработка команды /start
-async def start(message: Message):
-    connect = sqlite3.connect('gpt_us.db')        # Подключаемся к бд
-    cursor = connect.cursor()
-    sql_query = f"""
-                            SELECT * FROM ChatGPT WHERE id = '{int(message.chat.id)}'
-                            """
-    user = cursor.execute(sql_query).fetchone()
-    
-    if user == None:        # Проверяем, есть ли юзер в бд. Добавляем
-        sql_query = f"INSERT INTO ChatGPT(id, con, con2) VALUES ('{message.from_user.id}', '', '')"
-        cursor.execute(sql_query)
-         
-    await message.answer('Привет, это чат-бот на основе модели gpt 3.5🔥\nПриступим!')     # Приветствуем
-
-    connect.commit()        # Закрываем бд
-    connect.close()
-
-
-@dp.message_handler(commands='reset')    # Функция отчистки контекста в бд. 
-async def reset(message: Message):    
-    connect = sqlite3.connect('gpt_us.db')
-    cursor = connect.cursor()
-    sql_query = f"UPDATE ChatGPT SET con = '', con2 = '' WHERE id = {message.from_user.id}"
-    cursor.execute(sql_query)  
-    
-    await message.answer('✅История диалога отчищена✅')
-    
+                    )""")
     connect.commit()
     connect.close()
 
 
-@dp.message_handler(content_types=types.ContentType.TEXT)        # Обрабатываем запрос
-async def mes(message: types.Message): 
-    thread = threading.Thread(target=gpt, args=(message.text, message.from_user.id, message.message_id))        # Запускаем в новом потоке обработчик
-    thread.start()
-       
-    
-if __name__ == '__main__':
+@dp.message_handler(commands="start")
+async def start(message: Message) -> None:
+    """Обработчик команды /start"""
+
+    connect = sqlite3.connect("gpt_us.db", check_same_thread=False)
+    cursor = connect.cursor()
+
+    cursor.execute("SELECT * FROM ChatGPT WHERE id = ?", (message.from_user.id,))
+    user = cursor.fetchone()
+    if not user:  # Проверяем, есть ли юзер в бд. Добавляем
+        cursor.execute(
+            "INSERT INTO ChatGPT VALUES (?, ?, ?)", (message.from_user.id, "", "")
+        )
+
+    await message.answer("Привет, это чат-бот на основе модели gpt 3.5🔥\nПриступим!")  # Приветствуем
+
+    connect.commit()  # Закрываем соединение с бд
+    connect.close()
+
+
+@dp.message_handler(commands="reset")
+async def reset(message: Message) -> None:
+    """Функция отчистки контекста в бд"""
+
+    connect = sqlite3.connect("gpt_us.db", check_same_thread=False)
+    cursor = connect.cursor()
+    cursor.execute("DELETE FROM ChatGPT WHERE id = ?", (message.from_user.id,))
+    cursor.fetchone()
+    connect.commit()
+    connect.close()
+
+    await message.answer("✅История диалога отчищена✅")
+
+
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def mes(message: types.Message) -> None:
+    msg_accept = await message.answer(text="Генерируется ответ♻️")
+    gpt_answer = await gpt(text=message.text, user_id=message.from_user.id)
+    if not gpt_answer:
+        await msg_accept.edit_text(
+            text="❌ Что-то пошло не так. Попробуй снова через 30 секунд"
+        )
+        return
+    await msg_accept.delete()
+    await message.answer(text=gpt_answer)
+
+
+if __name__ == "__main__":
     create_table()
     executor.start_polling(dp)
